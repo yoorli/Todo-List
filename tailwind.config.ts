06c378c8f5a1f9615b6345cb0884e06d6c0441ce
yoolri <lucca6042@gmail.com>
2025-10-16 19:39:29 +0900

    chore: add Tailwind CSS configuration file (tailwind.config.ts)

---
+++ tailwind.config.ts
@@ -0,0 +1,41 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./src/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          50: 'var(--gray-50)',
+        },
+        slate: {
+          900: 'var(--slate-900)',
+          800: 'var(--slate-800)',
+          500: 'var(--slate-500)',
+          400: 'var(--slate-400)',
+          300: 'var(--slate-300)',
+          200: 'var(--slate-200)',
+          100: 'var(--slate-100)',
+        },
+        violet: {
+          600: 'var(--violet-600)',
+          100: 'var(--violet-100)',
+        },
+        rose: {
+          500: 'var(--rose-500)',
+        },
+        lime: {
+          300: 'var(--lime-300)',
+        },
+        amber: {
+          800: 'var(--amber-800)',
+        },
+      },
+      fontFamily: {
+        nanum: ['"NanumSquare"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
